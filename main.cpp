b1f5a8ecc98804c8241fefc38beebdc9420ac114
akalshamsi <akalshamsi@mail.csuchico.edu>
2018-03-01 19:34:14 -0800

    Project

--- main.cpp
+++ main.cpp
@@ -6,6 +6,23 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+int number;
+int success;
+
+  cout<<"How many times do you want to play?\n";
+  cin>>number;
+
+  for(int index=0; index<number; index++)
+  {
+      game.newGame();
+      game.guessDoorC();
+      game.guessDoorC();
+  
+  if(game.isWinner()==true)
+  {
+      success++;
+  }
+  }
+  cout<<"You have won "<<success<<endl;
   return 0;
 }
